441c2e8bbbfabbfd91490b32e0a0b7c8f1691318
Stian Surén <stian.suren@bekk.no>
2020-05-02 19:44:45 +0200

    Add express server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const app = require("express")();
-const server = require("http").Server(app);
-const io = require("socket.io")(server);
+const { createServer } = require("http");
+const { parse } = require("url");
+// const io = require("socket.io")(server);
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
@@ -9,31 +9,32 @@ const nextHandler = nextApp.getRequestHandler();
 
 let port = 3000;
 
-let players = [];
+// let players = [];
 
-io.on("connect", (socket) => {
-  const socketId = socket.id;
+// io.on("connect", (socket) => {
+//   const socketId = socket.id;
 
-  socket.on("player", (player) => {
-    players = [...players, player];
-    console.log(players);
-    io.emit("setPlayers", players);
-  });
+//   socket.on("player", (player) => {
+//     players = [...players, player];
+//     console.log(players);
+//     io.emit("setPlayers", players);
+//   });
 
-  socket.on("disconnect", () => {
-    players = players.filter((p) => p.id !== socketId);
-    console.log(players);
-    io.emit("setPlayers", players);
-  });
-});
+//   socket.on("disconnect", () => {
+//     players = players.filter((p) => p.id !== socketId);
+//     console.log(players);
+//     io.emit("setPlayers", players);
+//   });
+// });
 
 nextApp.prepare().then(() => {
-  app.get("*", (req, res) => {
-    return nextHandler(req, res);
-  });
-
-  server.listen(port, (err) => {
+  createServer((req, res) => {
+    // Be sure to pass `true` as the second argument to `url.parse`.
+    // This tells it to parse the query portion of the URL.
+    const parsedUrl = parse(req.url, true);
+    nextHandler(req, res, parsedUrl);
+  }).listen(3000, (err) => {
     if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
+    console.log("> Ready on http://localhost:3000");
   });
 });
